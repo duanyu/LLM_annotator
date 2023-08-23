97565b189d3844a6fc0c6dfa38cd1104ae79b42f
duanyu <duanyu027@qq.com>
2023-08-23 10:11:38 +0800

    add code

---
+++ experiment_vol3/init.sh
@@ -0,0 +1,8 @@
+pip3 install transformers_stream_generator
+pip3 install snorkel
+pip3 install paddlepaddle
+pip3 install paddlenlp
+mkdir dataset
+mkdir dataset/tnews
+mkdir exp_dataset
+mkdir exp_dataset/tnews
