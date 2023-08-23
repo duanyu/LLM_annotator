db158caf12bac46d5cf57152c140a127873db46f
duanyu <duanyu027@qq.com>
2023-08-23 10:11:11 +0800

    Delete experiment_vol1 directory

--- experiment_vol1/init.sh
+++
@@ -1,8 +0,0 @@
-pip3 install transformers_stream_generator
-pip3 install snorkel
-pip3 install paddlepaddle
-pip3 install paddlenlp
-mkdir dataset
-mkdir dataset/tnews
-mkdir exp_dataset
-mkdir exp_dataset/tnews
